cd51e596ea9754e8a1f1b8c087706a18b8572759
CrithCraft <25477690+CrithCraft@users.noreply.github.com>
2020-12-07 22:58:17 +0500

    Commenting

--- include/code.php
+++ include/code.php
@@ -4,25 +4,13 @@
     }
 </style>
 <?php
-    require 'db.php';
-
-    // Первая загрузка
-    $pdo = new PDO("mysql:host=localhost;dbname=pineforest_db;charset=utf8", $user, $pass);
-    $data = $pdo->prepare("SELECT * FROM source_background_data");
-    $data->execute();
-
-    $arPosts = array();
-    while ($row = $data->fetch(PDO::FETCH_ASSOC))
-        $arPosts[count($arPosts)] = $row;
-    $pdo = null;
-    // Завершение
-
+    // Поля навигации
     $nav_lables = ['home', 'photo', 'art'];
-    // $nav_lables = ['Home.php', 'Photos.php', 'Art.php'];
     $nav_lables_name = ['Главная', 'Фото', 'Арты'];
 
+    // Вход в учетную запись
     function login_db() {
-        $pdo = new PDO("mysql:host=localhost;dbname=pineforest_db;charset=utf8", $user, $pass);
+        $pdo = new PDO("mysql:host=localhost;dbname=pineforest_db;charset=utf8", 'fa052862', 'Kolombiana026V');
         $mailuid = $_POST['mailuid'];
         $password = $_POST['pwd'];
 
@@ -76,19 +64,19 @@
             
     }
 
+    // Выход из учетной записи
     function logout_db() {
-        //session_start();
         session_unset();
         session_destroy();
         $content = "four";
         $fp = fopen($_SERVER['DOCUMENT_ROOT'] . "/REGISTER_LOG.txt","a+");
         fwrite($fp,$content);
         fclose($fp);
-        //header("Location: ../Photos.php");
     }
 
+    // Регистрация учетной записи
     function singup_db(){
-        $pdo = new PDO("mysql:host=localhost;dbname=pineforest_db;charset=utf8", $user, $pass);
+        $pdo = new PDO("mysql:host=localhost;dbname=pineforest_db;charset=utf8", 'fa052862', 'Kolombiana026V');
         
         $username = $_POST['uid'];
         $email = $_POST['mail'];
@@ -125,15 +113,13 @@
                 die('<div style="background-color:white; display: flex; justify-content: center; align-items: center; height: 100%;"><p align="center" style="font-size: 16px; color: gray;">Вы успешно зарегистрированы! </br></br><a href="http://pineart.fo.ua/singin.php">Вернуться на предыдущую страницу.</a></p></div>');
                 exit();
             }
-
-            
-            
         }
     }
 
+    // Default функция возращает все данные из определенной таблицы базы базы данных в виде массива
     function sql_load($table){
         try {
-            $pdo = new PDO("mysql:host=localhost;dbname=pineforest_db;charset=utf8", $user, $pass);
+            $pdo = new PDO("mysql:host=localhost;dbname=pineforest_db;charset=utf8", 'fa052862', 'Kolombiana026V');
             $data = $pdo->prepare("SELECT * FROM ".$table);
             $data->execute();
             
@@ -155,6 +141,7 @@
         }
     }
 
+    // Генерация поста
     function renderBigPhotoBlockPage($page,$n, $type){
         if ($type == "photo"){
             $path = "images/backgrounds/";
@@ -216,38 +203,7 @@
         }
     }
 
-    function renderSmallPhotoBlock($a, $type){
-        if ($type == "photo") $photo_data = sql_load("source_background_data");
-        if ($type == "art") $photo_data = sql_load("source_art_data");
-        $i = 0; // После заполнения таблиц БД, убрать все это! Заменить k на i.
-        for ($k = 0; $k < $a; $k++){
-            $i = rand(0,count($photo_data)-1);
-            echo '  <div class="small-photo-block">
-                        <div class="photo-block-header">
-                            <div class = "photo-block-header-text">
-                                <img src="images/photo_icon.png" width="22px" height="22px" alt="icon">
-                                <h3>'.$photo_data[$i]['name'].'</h3>
-                                <div class = "author">'.$photo_data[$i]['author'].'</div>
-                                <div class = "date">'.$photo_data[$i]['date'].'</div>
-                            </div>
-                            <div class="photo-block-header-download">
-                                <img src="images/download_icon.png" width="20px" height="22px" alt="icon">
-                            </div>
-                        </div>
-                        <div style="background-image: url(images/backgrounds/'.$photo_data[$i]['image'].');" class="preview-photo">
-                            <img src="images/backgrounds/'.$photo_data[$i]['image'].'" width="100%" style="visibility: hidden;" />
-                        </div>
-                        <div class="hashtag-label">';
-                        hashtagDraw($photo_data[$i]['hashtag'], 2);
-                        echo '</div>
-                        <div class="location-label">
-                            <img src="images/location_icon.png" width="20px" height="20px" alt="icon">
-                            <div class="location-text">'.$photo_data[$i]['location'].'</div>
-                        </div>
-                    </div>';
-        }
-    }
-
+    // Генерация случайных хешетгов
     function renderHashtag($a){
         $hashtag_data = sql_load("source_hashtag_data");
         $i = 0; // После заполнения таблиц БД, убрать все это! Заменить k на i.
@@ -279,6 +235,7 @@
         echo '</form>';
     }
 
+    // Генерация хештегов поста
     function hashtagDraw($a,$n){
         $hashtags = explode ("/",$a);
         $hashtag_data = sql_load("source_hashtag_data");
@@ -296,6 +253,7 @@
         }
     }
 
+    // Удадение поста
     function removePicture($key, $id, $type){
         $type_db = "source_background_data";
         
@@ -308,7 +266,7 @@
         
         if (isset($_SESSION['userId'])){
             if ($data_photos[$id]['author'] == $_SESSION['userUid']) {
-                $pdo = new PDO("mysql:host=localhost;dbname=pineforest_db;charset=utf8", $user, $pass);
+                $pdo = new PDO("mysql:host=localhost;dbname=pineforest_db;charset=utf8", 'fa052862', 'Kolombiana026V');
                 $user = $data_photos[$id]['author'];
                 $data = $pdo->prepare("SELECT * FROM users WHERE uidUsers=?");
                 $data->bindParam(1, $user, PDO::PARAM_STR);
@@ -330,7 +288,7 @@
                         exit();
                     }
                     else if($pwdCheck == true) {
-                        $pdo = new PDO("mysql:host=localhost;dbname=pineforest_db;charset=utf8", $user, $pass);
+                        $pdo = new PDO("mysql:host=localhost;dbname=pineforest_db;charset=utf8", 'fa052862', 'Kolombiana026V');
                         $STH = $pdo->prepare("DELETE FROM ".$type_db." WHERE id =:id");
                         
                         $STH->bindParam(':id', $data_photos[$id]['id']);
@@ -352,6 +310,7 @@
         $pdo = null;
     }
 
+    // Отображение результатов поиска
     function renderSearchResult($search, $type){
         $posts = 0;
         if ($type == "photo"){
@@ -419,6 +378,7 @@
         }
     }
 
+    // Генерация поста
     function renderPageList ($page, $type, $page_size) {
         if ($type == "photo") $photo_data = sql_load("source_background_data");
         if ($type == "art") $photo_data = sql_load("source_art_data");
@@ -447,53 +407,13 @@
         echo '</div>';
     }
 
+    // Парсинг кординат
     function getCoord( $expr ) {
         $expr_p = explode( '/', $expr );
         return $expr_p[0] / $expr_p[1];
     }
 
-    function img_resize($src, $dest, $width, $height, $rgb=0xFFFFFF, $quality=100) {
-        if (!file_exists($src)) return false;
-
-        $size = getimagesize($src);
-
-        if ($size === false) return false;
-
-        // Определяем исходный формат по MIME-информации, предоставленной
-        // функцией getimagesize, и выбираем соответствующую формату
-        // imagecreatefrom-функцию.
-        $format = strtolower(substr($size['mime'], strpos($size['mime'], '/')+1));
-        $icfunc = "imagecreatefrom" . $format;
-        if (!function_exists($icfunc)) return false;
-
-        $x_ratio = $width / $size[0];
-        $y_ratio = $height / $size[1];
-
-        $ratio = min($x_ratio, $y_ratio);
-        $use_x_ratio = ($x_ratio == $ratio);
-
-        $new_width = $use_x_ratio ? $width : floor($size[0] * $ratio);
-        $new_height = !$use_x_ratio ? $height : floor($size[1] * $ratio);
-        $new_left = $use_x_ratio ? 0 : floor(($width - $new_width) / 2);
-        $new_top = !$use_x_ratio ? 0 : floor(($height - $new_height) / 2);
-
-        $isrc = $icfunc($src);
-        $idest = imagecreatetruecolor($width, $height);
-
-        imagefill($idest, 0, 0, $rgb);
-        imagecopyresampled($idest, $isrc, $new_left, $new_top, 0, 0,
-        $new_width, $new_height, $size[0], $size[1]);
-
-        imagejpeg($idest, $dest, $quality);
-
-        imagedestroy($isrc);
-        imagedestroy($idest);
-
-        return true;
-    }
-
-
-    // UPLOAD - OLD SCRIPT - 1 AM - Активируй осознано, без проверки код добавленя фото начинает конфликтовать с кнопками тегов
+    // Загрузка изображения на сайт
     $path_art = 'images/arts/';
     $path_sprite = 'images/sprites/';
     $path_background = 'images/backgrounds/';
@@ -536,27 +456,26 @@
         //Пока весьма не безопастно из-за возможности схватить вредоносный код из exif данных
         $img = $_FILES['upload-file']['tmp_name'];
         // Проверка поддерживаемого типа
-        // if (in_array($_FILES['upload-file']['type'], $types_exif)){
-        //     // Вытаскиваем данные изображения
-        //     $exif = exif_read_data( $img, 0, true );
-        //     if(!empty($exif['GPS'])){
-        //         // Ширина
-        //         $latitude['degrees'] = getCoord($exif['GPS']['GPSLatitude'][0]);
-        //         $latitude['degrees'] = floor($latitude['degrees']);
+        if (in_array($_FILES['upload-file']['type'], $types_exif)){
+            // Вытаскиваем данные изображения
+            $exif = exif_read_data( $img, 0, true );
+            if(!empty($exif['GPS'])){
+                // Ширина
+                $latitude['degrees'] = getCoord($exif['GPS']['GPSLatitude'][0]);
+                $latitude['degrees'] = floor($latitude['degrees']);
                 
-        //         // Долгота
-        //         $longitude['degrees'] = getCoord( $exif['GPS']['GPSLongitude'][0] );
-        //         $longitude['degrees'] = floor($longitude['degrees']);
+                // Долгота
+                $longitude['degrees'] = getCoord( $exif['GPS']['GPSLongitude'][0] );
+                $longitude['degrees'] = floor($longitude['degrees']);
 
-        //         // Локация
-        //         $location = $latitude['degrees']." / ".$longitude['degrees'];
+                // Локация
+                $location = $latitude['degrees']." / ".$longitude['degrees'];
 
-        //     }
-        //     else {
-        //         $location="Неопознано";
-        //     }
-        // }
- 
+            }
+            else {
+                $location="Неопознано";
+            }
+        }
 
         // Check type
         if (!in_array($_FILES['upload-file']['type'], $types))
@@ -583,7 +502,6 @@
             else {
                 $description = strip_tags($_POST["upload-photo-description"]);
             }
-
             if (!@copy($_FILES['upload-file']['tmp_name'], $path_background . $_FILES['upload-file']['name'])){
                 // Лог файл
                 // $errors= error_get_last();
@@ -594,34 +512,7 @@
                 die('<div style="background-color:white; grid-area:1/1/5/6; margin: auto 0;"><p align="center" style="font-size: 16px; color: gray;">Что-то пошло не так.</br></br><a href="?">Вернуться на страницу загрузки.</a></p></div>');
             }
             else {
-                $size = getimagesize($_FILES['upload-file']['tmp_name']);
-                list($width, $height, $type, $attr) = getimagesize($img);
-                if (!img_resize($path_background.$_FILES['upload-file']['name'], $path_background."low_".$_FILES['upload-file']['name'], 480, 480*$height/$width)){
-                    // Лог файл
-                    // $errors= error_get_last();
-                    // $file = fopen("log.txt", "w+");
-                    // fwrite($file, "COPY ERROR: ".$errors['type']."\n".$errors['message']);
-                    // fclose($file);
-                    // Уведомление об ошибке
-                    die('<div style="background-color:white; grid-area:1/1/5/6; margin: auto 0;"><p align="center" style="font-size: 16px; color: gray;">Что-то пошло не так.</br></br><a href="?">Вернуться на страницу загрузки.</a></p></div>');
-                }
-                else {
-                    
-                }
-                if (!img_resize($path_background.$_FILES['upload-file']['name'], $path_background."superlow_".$_FILES['upload-file']['name'], 480, 480*$height/$width)){
-                    // Лог файл
-                    // $errors= error_get_last();
-                    // $file = fopen("log.txt", "w+");
-                    // fwrite($file, "COPY ERROR: ".$errors['type']."\n".$errors['message']);
-                    // fclose($file);
-                    // Уведомление об ошибке
-                    die('<div style="background-color:white; grid-area:1/1/5/6; margin: auto 0;"><p align="center" style="font-size: 16px; color: gray;">Что-то пошло не так.</br></br><a href="?">Вернуться на страницу загрузки.</a></p></div>');
-                }
-                else {
-                    
-                }
-                
-                $pdo = new PDO("mysql:host=localhost;dbname=pineforest_db;charset=utf8", $user, $pass);
+                $pdo = new PDO("mysql:host=localhost;dbname=pineforest_db;charset=utf8", 'fa052862', 'Kolombiana026V');
                 $php = "INSERT INTO `source_background_data` (`id`, `name`, `author`, `date`, `image`, `description`, `location`, `hashtag`) VALUES (NULL,?,?,?,?,?,?,?);";
                 $STH = $pdo->prepare($php);
                 $STH->bindParam(1, strip_tags($_POST["upload-name"]), PDO::PARAM_STR);
@@ -655,7 +546,7 @@
                 die('<div style="background-color:white; grid-area:1/1/5/6; margin: auto 0;"><p align="center" style="font-size: 16px; color: gray;">Что-то пошло не так.</br></br><a href="?">Вернуться на страницу загрузки.</a></p></div>');
             }
             else {
-                $pdo = new PDO("mysql:host=localhost;dbname=pineforest_db;charset=utf8", $user, $pass);
+                $pdo = new PDO("mysql:host=localhost;dbname=pineforest_db;charset=utf8", 'fa052862', 'Kolombiana026V');
                 $php = "INSERT INTO `source_art_data` (`id`, `name`, `author`, `date`, `image`, `description`, `location`, `hashtag`) VALUES (NULL,?,?,?,?,?,?,?);";
                 $STH = $pdo->prepare($php);
                 $STH->bindParam(1, strip_tags($_POST["upload-name"]), PDO::PARAM_STR);
